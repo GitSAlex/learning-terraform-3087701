3d138969d5ab05b8f2b1c663316cca596e127bc8
GitSAlex <127146866+GitSAlex@users.noreply.github.com>
2024-01-03 17:11:58 +0200

    web

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "blog_vpc" {
+module "web_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -30,36 +30,36 @@ module "blog_vpc" {
 }
 
 
-module "blog_autoscaling" {
+module "web_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name = "blog"
+  name = "web"
 
   min_size            = 1
   max_size            = 2
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
+  vpc_zone_identifier = module.web_vpc.public_subnets
+  target_group_arns   = module.web_alb.target_group_arns
+  security_groups     = [module.web_sg.security_group_id]
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
 }
 
-module "blog_alb" {
+module "web_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "blog-alb"
+  name = "web-alb"
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
+  vpc_id             = module.web_vpc.vpc_id
+  subnets            = module.web_vpc.public_subnets
+  security_groups    = [module.web_sg.security_group_id]
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "web-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -79,12 +79,12 @@ module "blog_alb" {
   }
 }
 
-module "blog_sg" {
+module "web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
+  vpc_id  = module.web_vpc.vpc_id
+  name    = "web"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
