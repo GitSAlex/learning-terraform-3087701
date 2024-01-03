2214f9d83109d83466ecb2f1f52f119f45aec0f4
GitSAlex <127146866+GitSAlex@users.noreply.github.com>
2024-01-03 15:37:08 +0200

    g

--- main.tf
+++ main.tf
@@ -18,50 +18,50 @@ data "aws_vpc" "default"{
   default = true
 }
 
-resource "aws_instance" "blog" {
+resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vcp_security_group_ids = [aws_security_group.blog.id]
+  vcp_security_group_ids = [aws_security_group.web.id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
-resource "aws_security_group" "blog" {
-  name        = "blog"
+resource "aws_security_group" "web" {
+  name        = "web"
   description = "Allow http and https in. Allow everything out."
 
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "web_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+  aws_security_group_id = aws_security_group.web.id
 }
 
-resource "aws_security_group_rule" "blog_https_in" {
+resource "aws_security_group_rule" "web_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+  aws_security_group_id = aws_security_group.web.id
 }
 
-resource "aws_security_group_rule" "blog_everything_out" {
+resource "aws_security_group_rule" "web_everything_out" {
   type        = "ingress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+  aws_security_group_id = aws_security_group.web.id
 }
