50ec378ffe7132b50fada21b0b7ff10bf45b4756
GitSAlex <127146866+GitSAlex@users.noreply.github.com>
2024-01-03 16:19:16 +0200

    new dev npc

--- main.tf
+++ main.tf
@@ -18,11 +18,28 @@ data "aws_vpc" "default"{
   default = true
 }
 
+module "web_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.web_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
@@ -34,48 +51,11 @@ module "web_sg" {
   version = "5.1.0"
   name = "web_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.web_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp" , "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "web" {
-  name        = "web"
-  description = "Allow http and https in. Allow everything out."
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "web_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_everything_out" {
-  type        = "ingress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
 }
